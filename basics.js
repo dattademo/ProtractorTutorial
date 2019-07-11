ac75de618d1415f78e2244cf6ec46664cbd4232b
Damian Attademo <damian.attademo@wwecorp.com>
2019-07-11 15:53:53 -0300

    First Commit

---
+++ basics.js
@@ -0,0 +1,7 @@
+console.log("Hello world");
+
+var a = 4;
+var b = "hello";
+
+console.log(a);
+console.log(b);
---
+++ conf.js
@@ -0,0 +1,13 @@
+// conf.js
+exports.config = {
+	directConnect: true,
+	framework: 'jasmine',
+
+	specs: ['todo-spec.js'],
+	capabilities: {
+		browserName: 'chrome'
+	}
+}
+
+
+
